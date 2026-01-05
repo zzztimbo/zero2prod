af2292e1051f42c32226dca89a911fcbac5f8dd4
Tim Chan <tim@chan.net>
2026-01-05 09:52:19 -0800

    embedded test module (#3)

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,36 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
     format!("Hello {}!", &name)
 }
 
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    env_logger::init();
+
     HttpServer::new(|| {
         App::new()
+            .route("/health_check", web::get().to(health_check))
             .route("/", web::get().to(greet))
             .route("/{name}", web::get().to(greet))
     })
     .bind("127.0.0.1:8000")?
     .run()
     .await
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::health_check;
+
+    #[tokio::test]
+    async fn health_check_succeeds() {
+        let response = health_check().await;
+        assert!(response.status().is_success())
+    }
 }
