7469ab703d0648296670fe120a041cc3a79b987c
Tim Chan <tim@chan.net>
2026-01-05 15:27:15 -0800

    refactor how spawn app works so that it returns the full app url instead of just the port (#6)

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,14 +1,11 @@
 #[tokio::test]
 async fn health_check_succeeds() {
-    let port = spawn_app();
+    let app_url = spawn_app();
 
     let client = reqwest::Client::new();
 
     let response = client
-        .get(format!(
-            "http://localhost:{}/health_check",
-            port.to_string()
-        ))
+        .get(format!("{}/health_check", app_url))
         .send()
         .await
         .unwrap();
@@ -16,11 +13,11 @@ async fn health_check_succeeds() {
     assert!(response.status().is_success());
 }
 
-fn spawn_app() -> u16 {
+fn spawn_app() -> String {
     let listener = std::net::TcpListener::bind("127.0.0.1:0").expect("Failed to bind");
     let port = listener.local_addr().unwrap().port();
     println!("Running on port: {}", port);
     let server = zero2prod::run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
-    port
+    format!("http://127.0.0.1:{}", port)
 }
