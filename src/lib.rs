2e2f8733e344c62d07e70605059b66ddfb7ca9e1
Tim Chan <tim@chan.net>
2026-01-06 09:42:49 -0800

    implement subscriptions endpoint and include tests for success and error

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,30 @@
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpResponse, HttpServer};
+use serde::Deserialize;
 
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
+#[derive(Deserialize)]
+#[allow(dead_code)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 pub fn run(listener: std::net::TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
 
     Ok(server)
 }
